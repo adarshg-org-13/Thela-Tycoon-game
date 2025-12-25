e6a6f2b81ec4e82504fe38f7d103afbe205d9446
adarshg-org-13 <https://github.com/adarshg-org-13/Whispers-of-the-pertrified-path>
2025-12-25 20:42:06 +0530

    working on player movement

---
+++ Main_player_movement_s.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const walk_speed = 200
+const run_speed = 350
+
+var current_speed = walk_speed
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left","right","forward","back")
+	
+	if  Input.is_action_pressed("run"):
+		current_speed = run_speed
+	else:
+		current_speed = walk_speed
+	if direction:
+		velocity.x= direction.x * current_speed
+		velocity.y = direction.y * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.y = move_toward(velocity.y, 0, current_speed)
+	move_and_slide()
