466e9f5defe6c86f65f22619a0d62a7fef8fb3ba
adarshg-org-13 <https://github.com/adarshg-org-13/Whispers-of-the-pertrified-path>
2025-12-28 19:54:28 +0530

    added a lot of scenes in the game and fixed some scenes

--- Main_player_movement_s.gd
+++ Main_player_movement_s.gd
@@ -1,21 +1,39 @@
 extends CharacterBody2D
 
-const walk_speed = 250
-const run_speed = 400
+const WALK_SPEED = 250
+const RUN_SPEED = 400
 
-var current_speed = walk_speed
+var current_speed = WALK_SPEED
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	var direction = Input.get_vector("left","right","forward","back")
+	var direction = Input.get_vector("left", "right", "forward", "back")
 	
-	if  Input.is_action_pressed("run"):
-		current_speed = run_speed
+	if Input.is_action_pressed("run"):
+		current_speed = RUN_SPEED
 	else:
-		current_speed = walk_speed
+		current_speed = WALK_SPEED
+
 	if direction:
-		velocity.x= direction.x * current_speed
-		velocity.y = direction.y * current_speed
+		velocity = direction * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.y = move_toward(velocity.y, 0, current_speed)
+		velocity = velocity.move_toward(Vector2.ZERO, current_speed)
+	
 	move_and_slide()
+	update_animations(direction)
+
+func update_animations(direction):
+	if direction == Vector2.ZERO:
+		animated_sprite.play("Idle animation")
+		return 
+	if direction.y < 0:
+		animated_sprite.play("Up animation")
+	elif direction.y > 0:
+		animated_sprite.play("Down animation")
+	else:
+		if direction.x != 0:
+			animated_sprite.flip_h = direction.x > 0
+		if current_speed == RUN_SPEED:
+			animated_sprite.play("Running animation")
+		else:
+			animated_sprite.play("Walk animation")
