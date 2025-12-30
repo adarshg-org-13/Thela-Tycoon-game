6f201d4029edb70b1cfb94450e7d7cb27f0ccc19
adarshg-org-13 <https://github.com/adarshg-org-13/Whispers-of-the-pertrified-path>
2025-12-30 18:57:11 +0530

    added a lot of things but also find some bugs

---
+++ scene_shop.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_esc_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://main_shops.tscn")
+
+
+func _on_esc_ms_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://main_shops.tscn")
+
+
+func _on_esc_ws_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://main_shops.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Escape"):
+		get_tree().change_scene_to_file("res://main_shops.tscn")
